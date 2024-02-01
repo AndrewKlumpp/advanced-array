c81b06acc915dbfdbc7573260b900ace37cc8958
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-01-31 22:23:20 -0700

    finished avgVal with passing mocha test

--- problems/01-avg-val-forEach.js
+++ problems/01-avg-val-forEach.js
@@ -6,16 +6,24 @@ it should return `null`.
 Solve this using Array's `forEach()` method.
 
 Examples:
+
+console.log(avgVal([5, 10])); // 7.5
+console.log(avgVal([3, 7, 2, 1, 2])); // 3
+console.log(avgVal([])); // null
 */
 
-// console.log(avgVal([5, 10])); // 7.5
-// console.log(avgVal([3, 7, 2, 1, 2])); // 3
-// console.log(avgVal([])); // null
-
-
 let avgVal = function(arr) {
-    // Your code here
+  if (arr.length === 0) {
+    return null;
+  }
+  let sum = 0;
+  arr.forEach(function (value) {
+    sum += value;
+  });
+  let average = sum / (arr.length);
+  return average;
 };
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
