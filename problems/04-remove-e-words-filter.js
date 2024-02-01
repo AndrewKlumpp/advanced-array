efcd3278e1db07d8a29b131f68d1dce5bb6bad3c
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-01 11:52:24 -0700

    finished removeEWords with passing mocha tests

--- problems/04-remove-e-words-filter.js
+++ problems/04-remove-e-words-filter.js
@@ -13,7 +13,11 @@ console.log(removeEWords('Enter the building')); // 'building'
 */
 
 let removeEWords = function(sentence) {
-    // Your code here
+    const words = sentence.split(' ');
+    const noEWords = words.filter(function (word) {
+        return !word.toLowerCase().includes('e');
+    });
+    return noEWords.join(' ');
 };
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
@@ -22,4 +26,4 @@ try {
   module.exports = removeEWords;
 } catch (e) {
   module.exports = null;
-}
+}
