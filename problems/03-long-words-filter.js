930afc5a0c2d23ce778c9b757c903e4d968777b9
AndrewKlumpp <AndrewOzcyKlumpp@gmail.com>
2024-02-01 11:35:32 -0700

    finished longWords with passing mocha test

--- problems/03-long-words-filter.js
+++ problems/03-long-words-filter.js
@@ -14,8 +14,12 @@ console.log(longWords(['couscous', 'soup', 'ceviche', 'solyanka' ,'taco'])); // 
 */
 
 let longWords = function(words) {
-    // Your code here
+    const fiveWords = words.filter(function (word) {
+        return word.length > 5;
+    });
+    return fiveWords;
 };
+
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 
@@ -23,4 +27,4 @@ try {
   module.exports = longWords;
 } catch (e) {
   module.exports = null;
-}
+}
